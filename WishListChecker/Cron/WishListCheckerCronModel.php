3d210eab0a68f43f6a3176a24293210625f86d09
iammerlin <memcached7@gmail.com>
2022-06-27 03:30:27 +0300

    V1.0.1

--- WishListChecker/Cron/WishListCheckerCronModel.php
+++ WishListChecker/Cron/WishListCheckerCronModel.php
@@ -2,10 +2,33 @@
 
 namespace Astound\WishListChecker\Cron;
 
+use Magento\Framework\App\ObjectManager;
+
 class WishListCheckerCronModel
 {
-    public function execute()
-    {
-         //ваш код крон задачі
+    protected $_salesOrderCollection;
+
+    private $_logger;
+
+    public function __construct(
+        \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $salesOrderCollection,
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->_salesOrderCollection = $salesOrderCollection;
+        $this->_logger = $logger;
+    }
+
+    public function execute() {
+        $result = [];
+
+        $collection = $this->_salesOrderCollection->create();
+
+        foreach ($collection as $order) {
+            foreach ($order->getAllVisibleItems() as $item) {
+                $result[] = $item->getSku();
+            }
+        }
+        $this->_logger->info(var_dump($result));
+        return $result;
     }
 }
