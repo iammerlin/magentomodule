413fcb76417716805bad514a91b8a4329d871abf
iammerlin <memcached7@gmail.com>
2022-06-30 21:48:53 +0300

    V1.0.6

--- WishListChecker/Cron/NotifyCustomerCronModel.php
+++ WishListChecker/Cron/NotifyCustomerCronModel.php
@@ -4,44 +4,27 @@ namespace Astound\WishListChecker\Cron;
 
 use Magento\Framework\App\ObjectManager;
 use Astound\WishListChecker\Model\ProxyQuoteFactory;
-use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Framework\Mail\TransportInterfaceFactory;
 
 class NotifyCustomerCronModel
 {
-	protected $_productRepository;
-    private $_logger;
-    private $transportBuilder;
+    protected $mailTransportFactory;
 
     public function __construct(
-        \Magento\Catalog\Model\ProductRepository $productRepository,
-        \Psr\Log\LoggerInterface $logger,
-        \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder
-    ) {
-        $this->_productRepository = $productRepository;
-        $this->_logger = $logger;
-        $this->transportBuilder = $transportBuilder;
+        TransportInterfaceFactory $mailTransportFactory
+    )
+    {
+        $this->mailTransportFactory = $mailTransportFactory;
     }
 
-    public function execute() {
-        $result = [];
-
-        // $product = $this->_productRepository->getById("1562");
-        // var_dump($product->getName());
-
-        try {
-            $templateVars = [];
-            $transport = $this->transportBuilder->setTemplateIdentifier('59')
-            ->setTemplateOptions( [ 'area' => \Magento\Framework\App\Area::AREA_FRONTEND, 'store' => 1 ] )
-            ->setTemplateVars( $templateVars )
-            ->setFrom( [ "name" => "Magento ABC CHECK PAYMENT", "email" => "memcached7@gmail.com" ] )
-            ->addTo('memcached7@gmail.com')
-            ->setReplyTo('memcached7@gmail.com')
-            ->getTransport();
-            $transport->sendMessage(); 
-        } catch (Exception $e) {
-            $this->logger->error($e);
-        }
-
-        return $result;
+    public function execute()
+    {
+        $message = new \Magento\Framework\Mail\Message();
+        $message->setFrom('mail.recipient@example.com');
+        $message->addTo('mail.tosent@example.com');
+        $message->setSubject('Subject');
+        $message->setBody('Body');
+        $transport = $this->mailTransportFactory->create(['message' => $message]);
+        $transport->sendMessage();
     }
 }
